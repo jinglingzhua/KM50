31553d123241ae37e57fa5d6e266e7204fbd7801
开饭了 <767748788@qq.com>
2025-12-07 14:04:03 +0800

    Add files via upload

---
+++ config.h
@@ -0,0 +1,78 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+/* Select hand configuration */
+
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+// #define PERMISSIVE_HOLD
+
+#define HOLD_ON_OTHER_KEY_PRESS
+#define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
+#define TAPPING_TERM 200
+// #define TAPPING_TERM_PER_KEY
+#define TAPPING_TOGGLE 3
+#define QUICK_TAP_TERM_PER_KEY
+
+// #define QUICK_TAP_TERM 0
+
+#undef RGBLED_NUM
+#define RGBLIGHT_ANIMATIONS
+#define RGBLED_NUM 27
+#define RGBLIGHT_LIMIT_VAL 120
+#define RGBLIGHT_HUE_STEP 10
+#define RGBLIGHT_SAT_STEP 17
+#define RGBLIGHT_VAL_STEP 17
+
+// #undef MOUSEKEY_DELAY
+// #define MOUSEKEY_DELAY          10
+// #undef MOUSEKEY_INTERVAL
+// #define MOUSEKEY_INTERVAL       16
+// #undef MOUSEKEY_WHEEL_DELAY
+// #define MOUSEKEY_WHEEL_DELAY    10
+// #undef MOUSEKEY_MAX_SPEED
+// #define MOUSEKEY_MAX_SPEED      6
+// #undef MOUSEKEY_TIME_TO_MAX
+// #define MOUSEKEY_TIME_TO_MAX    64
+#define MK_3_SPEED
+#define MK_MOMENTARY_ACCEL
+// #define MK_C_OFFSET_0 4
+// #define MK_C_INTERVAL_0 16
+// #define MK_C_OFFSET_1 8
+// #define MK_C_INTERVAL_1 16
+// #define MK_C_OFFSET_2 16
+// #define MK_C_INTERVAL_2 16
+
+// #define MK_KINETIC_SPEED
+// #define MOUSEKEY_INITIAL_SPEED 400
+// #define MOUSEKEY_WHEEL_INITIAL_MOVEMENTS 4
+// #define MOUSEKEY_BASE_SPEED 2000
+
+// Underglow
+/*
+#undef RGBLED_NUM
+#define RGBLED_NUM 14    // Number of LEDs
+#define RGBLIGHT_ANIMATIONS
+#define RGBLIGHT_SLEEP
+*/
